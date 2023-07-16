21bfdada5b33f6e46a63a6fc14412d1deb12568b
Lukas Lück <134540923+LukasLueck@users.noreply.github.com>
2023-07-16 10:45:18 +0200

    Add files via upload
    Stand vom 16.07.2023 / 10:45 Uhr

---
+++ EA2/translations/translation_en_US.ts
@@ -0,0 +1,150 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hallo Kinder!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hallo Kinder!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Starten wir mit den Übungen.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Starten wir mit den Übungen.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Lasst uns mit den Übungen starten.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Lasst uns mit den Übungen starten.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (10)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Und danach gehen wir wieder in unsere Ausgangposition.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Und danach gehen wir wieder in unsere Ausgangposition.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (11)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Das habt ihr schonmal richtig gut gemacht!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Das habt ihr schonmal richtig gut gemacht!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Übung 1.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Übung 1.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Wir strecken unsere Arme über den Kopf.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Wir strecken unsere Arme über den Kopf.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Wir halten unsere Arme über dem Kopf.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Wir halten unsere Arme über dem Kopf.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (5)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Und dann lassen wir die Arme wieder runter.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Und dann lassen wir die Arme wieder runter.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (6)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Übung 2.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Übung 2.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (7)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Wir stehen gerade auf beiden Füßen. Das ist immer unsere Grundhaltung.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Wir stehen gerade auf beiden Füßen. Das ist immer unsere Grundhaltung.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (8)</name>
+        <message>
+            <source>Jetzt stellen wir uns auf das rechte Bein.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Jetzt stellen wir uns auf das rechte Bein.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Jetzt stellen wir uns auf das linke Bein.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Jetzt stellen wir uns auf das linke Bein.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (9)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Versucht mal so auf dem Bein stehen zu bleiben.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Versucht mal so auf dem Bein stehen zu bleiben.</translation>
+        </message>
+    </context>
+</TS>
